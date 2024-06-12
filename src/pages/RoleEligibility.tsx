44c643aed0214bd40f0ad1d637321c32e2b52314
Alex Day <aday@hey.com>
2024-06-12 18:40:23 +0100

    role / skills toggle

--- src/pages/RoleEligibility.tsx
+++ src/pages/RoleEligibility.tsx
@@ -1,32 +1,51 @@
 // Home.tsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Resource } from "../types";
-import { getResource } from "../handlers/apiHandlers";
+import { Resource, Role } from "../types";
+import { getResource, getResourceRoleEligibility } from "../handlers/apiHandlers";
 import Button from "../components/Button";
 import styles from "./roleEligibility.module.css"
 
 const RoleEligibility: React.FC = () => {
   const [resource, setResource] = useState<Resource>();
+  const [availability, setAvailability] = useState<Role[]>([]);
   const [eligible, setEligible] = useState<boolean>(true);
   const [skills, setSkills] = useState<boolean>(false);
   const { id } = useParams<{ id: string }>();
-    
-    useEffect(() => {
-        const fetchResource = async () => {
-          if (!id) {
-            console.error("Resource ID is undefined.");
-            return;
-          }
-            try {
-                const response = await getResource(id);
-                const data = await response.json();
-                setResource(data);
-            } catch (error) {
-                console.error("Error fetching resources:", error);
-            }
-        };
+  
+  useEffect(() => {
+    const fetchResource = async () => {
+      if (!id) {
+        console.error("Resource ID is undefined.");
+        return;
+      }
+      try {
+        const response = await getResource(id);
+        const data = await response.json();
+        setResource(data);
+      } catch (error) {
+        console.error("Error fetching resources:", error);
+      }
+    };
     fetchResource();
+}, [id]);
+
+
+  useEffect(() => {
+    const fetchAvailability = async () => {
+      if (!id) {
+        console.error("Resource ID is undefined.");
+        return;
+      }
+        try {
+          const response = await getResourceRoleEligibility(id);
+          const data = await response.json();
+          setAvailability(data);
+        } catch (error) {
+            console.error("Error fetching resources:", error);
+        }
+    };
+    fetchAvailability();
   }, [id]);
 
   const toggleStates = () => {
@@ -47,7 +66,11 @@ const RoleEligibility: React.FC = () => {
         </div>
         
         {eligible && 
-        <div>Eligible!</div>
+        <div>{availability && availability.map((role) => (
+          <li key={role.id}>
+            {role.name}
+            </li>
+        ))}</div>
         }
 
 {skills && 
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,13 @@ export interface Resource {
     id: string;
     name: string;
   }
+  
+export interface Role {
+    id: number;
+    name: string;
+    skillsRequired: [{
+        id: number;
+        name: string;
+        hasSkill: boolean;
+    }]
+  }
