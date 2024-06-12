b397000e744c444265320c4ac1a50d70f7cb7cb6
Alex Day <aday@hey.com>
2024-06-12 15:50:48 +0100

    api handler functions

--- api/controllers/skills.js
+++
@@ -1,5 +0,0 @@
-const db = require("../db");
-
-exports.getSkills = async (req, res, next) => {
-  res.status(200).json(db.skills);
-};
--- api/routes/skills.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const dataController = require("../controllers/skills");
-
-router.get("/", dataController.getSkills);
-
-module.exports = router;
